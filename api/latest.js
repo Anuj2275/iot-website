190874ebb9fb626fa73d96a2e7b90f5df800336d
Anuj2275 <156070503+Anuj2275@users.noreply.github.com>
2025-04-19 12:20:45 +0530

    Update latest.js

--- api/latest.js
+++ api/latest.js
@@ -1,8 +1,12 @@
-export default async (req, res) => {
-    try {
-        const data = await kv.get('latestData');
-        return res.status(200).json(JSON.parse(data || '{}'));
-    } catch (error) {
-        return res.status(500).json({ error: 'Data retrieval failed' });
-    }
-};
+export default function handler(req, res) {
+  res.status(200).json({ hello: "world" });
+}
+
+// export default async (req, res) => {
+//     try {
+//         const data = await kv.get('latestData');
+//         return res.status(200).json(JSON.parse(data || '{}'));
+//     } catch (error) {
+//         return res.status(500).json({ error: 'Data retrieval failed' });
+//     }
+// };
